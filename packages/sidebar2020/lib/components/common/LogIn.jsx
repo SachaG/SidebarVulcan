3dcd071ca8d0b1a8de0074ace62f2da83dc1dcd7
SachaG <358832+SachaG@users.noreply.github.com>
2020-09-06 18:42:39 +0900

    Log in form

--- packages/sidebar2020/lib/components/common/LogIn.jsx
+++ packages/sidebar2020/lib/components/common/LogIn.jsx
@@ -1,4 +1,4 @@
-import { Components, useCurrentUser } from 'meteor/vulcan:core';
+import { Components, useCurrentUser, getSetting } from 'meteor/vulcan:core';
 import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import qs from 'qs';
@@ -8,8 +8,7 @@ import { useApolloClient } from '@apollo/react-hooks';
 import { useCookies } from 'react-cookie';
 
 // Sidebar's OneGraph appId
-// const APP_ID = 'ab70c14a-a1e0-4f9b-9a38-5ebda9792f3f';
-const APP_ID = '9550429e-f67e-46d8-8722-5682336ed191';
+const APP_ID = getSetting('oneGraph.appId');
 
 // OneGraphAuth uses the window object to display the popup, we need to check it exists due to SSR.
 export const auth =
@@ -61,18 +60,10 @@ const LogIn = () => {
               login({
                 service: 'twitter',
                 onLoggedIn: async () => {
-                  // eslint-disable-next-line
-                  // eslint-disable-next-line
                   const authToken = auth.accessToken().accessToken;
-                  console.log('\tauth!', authToken);
-                  console.log(client)
                   setCookie('og_auth_token', authToken);
-                  console.log("Set the cookie to ", authToken);
-                  const z = cookies['og_auth_token'];
-                  console.log("Got the cookie: ", z);
-
                   await client.resetStore();
-                  // history.push(`/log-in/finish/?redirect=${redirect}`);
+                  history.push(`/log-in/finish/?redirect=${redirect}`);
                 },
               });
             }}
